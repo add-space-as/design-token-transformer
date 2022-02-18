f906d0d801ffc78a0c0a3fc5af92d755c71e02f6
lukasoppermann <lukasoppermann@users.noreply.github.com>
2022-02-18 09:50:11 +0000

    Tokens updated

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -4,12 +4,28 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Fri, 18 Feb 2022 09:50:11 GMT
 
 
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+BordersSingleStroke,
+BordersDashedOutsideStroke,
+BordersSingleStyleStroke,
+BordersUnsupportedMultipleBordersStroke,
+GradientGradientSingleWithMultipleColorStopsStops0Color,
+GradientGradientSingleWithMultipleColorStopsStops1Color,
+GradientGradientSingleWithMultipleColorStopsStops2Color,
+GradientGradientSingleWithMultipleColorStopsStops3Color,
+GradientGradientMultiple0Stops0Color,
+GradientGradientMultiple0Stops1Color,
+GradientGradientMultiple1Stops0Color,
+GradientGradientMultiple1Stops1Color,
+GradientGradientMultiple2Stops0Color,
+GradientGradientMultiple2Stops1Color,
+GradientGradientMultiple3Stops0Color,
+GradientGradientMultiple3Stops1Color,
 ColorColorsMultipleFills0,
 ColorColorsMultipleFills1,
 ColorColorsSingleBlue,
@@ -17,7 +33,11 @@ ColorColorsRefBlue,
 ColorColorsSpecialCharacters,
 ColorColorsSpecialCharactersNderung,
 ColorLightBackground,
-ColorDarkBackground
+ColorDarkBackground,
+EffectDropShadowSingleColor,
+EffectInnerShadowMultiple0Color,
+EffectInnerShadowMultiple1Color,
+EffectInnerShadowMultiple2Color
 };
 
 @interface StyleDictionaryColor : NSObject
--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 16 Dec 2021 18:06:29 GMT
+// Generated on Fri, 18 Feb 2022 09:50:11 GMT
 
 
 #import "StyleDictionaryColor.h"
@@ -21,14 +21,34 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
+rgba(0, 0, 0, 1),
+rgba(0, 0, 0, 1),
+rgba(4, 74, 255, 1),
+rgba(255, 230, 0, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 138, 0, 1),
+rgba(255, 46, 0, 1),
+rgba(255, 0, 0, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 255, 255, 1),
+rgba(255, 255, 255, 1),
+rgba(207, 48, 48, 1),
+rgba(255, 255, 255, 1),
+rgba(74, 79, 204, 1),
+rgba(255, 255, 255, 1),
 [UIColor colorWithRed:0.251f green:1.000f blue:0.729f alpha:1.000f],
-[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:0.102f],
+[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:0.100f],
 [UIColor colorWithRed:0.016f green:0.290f blue:1.000f alpha:1.000f],
 [UIColor colorWithRed:0.016f green:0.290f blue:1.000f alpha:1.000f],
 [UIColor colorWithRed:0.251f green:0.875f blue:0.314f alpha:1.000f],
 [UIColor colorWithRed:0.204f green:0.337f blue:0.686f alpha:1.000f],
 [UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.000f],
-[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f]
+[UIColor colorWithRed:0.000f green:0.000f blue:0.000f alpha:1.000f],
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 1),
+rgba(0, 0, 0, 0.25)
     ];
   });
 
