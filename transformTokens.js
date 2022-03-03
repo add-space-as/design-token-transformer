93c7c8050247a02cee1e3ca6ddd9d9ae9e0d8585
Peter Szots <peter.szots@apps.no>
2022-03-03 13:57:54 +0100

    Add Flutter support

--- transformTokens.js
+++ transformTokens.js
@@ -1,5 +1,6 @@
 const StyleDictionary = require('style-dictionary')
 const baseConfig = require('./config.json')
+const Color = require('tinycolor2')
 
 StyleDictionary.registerTransform({
   name: 'size/px',
@@ -52,6 +53,18 @@ StyleDictionary.registerFilter({
   matcher: function(token) {
     return ['dimension', 'string', 'number', 'color'].includes(token.type)
   }
+})
+
+StyleDictionary.registerTransform({
+    name: 'colors/hex8flutter',
+    type: 'value',
+    matcher: prop => {
+        return prop.attributes.category === 'colors'
+    },
+    transformer: prop => {
+        var str = Color(prop.value).toHex8().toUpperCase();
+        return `Color(0x${str.slice(6)}${str.slice(0, 6)})`;
+    },
 })
 
 const StyleDictionaryExtended = StyleDictionary.extend(baseConfig)
